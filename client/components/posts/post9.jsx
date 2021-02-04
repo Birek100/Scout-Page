3c9f34020b31cf64bc21015b64e14cf71ccccf6f
Bartosz Szura <bartosz.szura100@gmail.com>
2021-02-04 22:41:41 +0100

    add first modal box image

--- client/components/posts/post9.jsx
+++ client/components/posts/post9.jsx
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 
 function Post9() {
+  const imgRef = useRef(null);
+  const modalRef = useRef(null);
+  const modalImgRef = useRef(null);
+
+  const [modalState, setModalState] = useState(false);
+  const [imgState, setImgState] = useState(null);
+  const openModal = () => {
+    console.log(imgRef.current.currentSrc);
+    setModalState(true);
+    setImgState(imgRef.current.currentSrc);
+  };
+  const closeModal = () => {
+    setModalState(false);
+    setImgState(null);
+  };
+
+  const outsideClick = e => {
+    if (e.target.className === "modal--active") {
+      closeModal();
+    }
+  };
+
   return (
     <div className="wrapper">
       <div className="wrapper__inside">
+        <div
+          id="myModal"
+          className={modalState ? "modal--active" : "modal"}
+          ref={modalRef}
+          onClick={e => outsideClick(e)}
+        >
+          <span className="close" onClick={closeModal}>
+            &times;
+          </span>
+          <img
+            id="modalimg"
+            className="modal-content"
+            src={imgState}
+            ref={modalImgRef}
+          />
+        </div>
+
         <div className="content">
           <div className="content__article">
             <h2>Zbiórka żywności 14 - 16 grudnia</h2>
           </div>
           <div className="content__image">
-            <img src="./static/images/post_images/fotokonkurs.jpg" />
+            <img
+              src="./static/images/post_images/fotokonkurs.jpg"
+              id="myimg"
+              ref={imgRef}
+              onClick={openModal}
+            />
           </div>
           <div className="content__article">
             <p>
-              <span classNAme="bold">
+              <span className="bold">
                 Zapraszamy do udziału w konkursie fotograficznym Harcerska Zima
               </span>
             </p>
